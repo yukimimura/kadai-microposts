c103565fe169a759375844105ecc3105b082dd71
yuki.mimura <perusona5excuse@yahoo.co.jp>
2020-03-23 10:48:57 +0000

    timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   
-  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -26,5 +25,9 @@ class User < ApplicationRecord
 
   def following?(other_user)
     self.followings.include?(other_user)
+  end
+  
+  def feed_microposts
+    Micropost.where(user_id: self.following_ids + [self.id])
   end
 end
