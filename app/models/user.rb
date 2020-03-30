fc41306ed0944c02aa2179f61441c23733b95e32
yuki.mimura <perusona5excuse@yahoo.co.jp>
2020-03-30 08:32:00 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -30,4 +31,23 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  
+    
+  has_many :favorites
+  has_many :myfavorites, through: :favorites, source: :micropost
+  
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  def unfavorite(micropost)
+    fav = self.favorites.find_by(micropost_id: micropost.id)
+    fav.destroy if fav
+  end
+
+  def myfavorites?(micropost)
+    self.myfavorites.include?(micropost)
+  end
+  
 end
